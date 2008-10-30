9e1a8461341a698f0209cf1840330b61c62c7ca6
Alexey Kovyrin <alexey@kovyrin.net>
2008-10-29 23:22:39 -0400

    Backport our changes to loops plugin

--- generators/loops/templates/script/loops.rb
+++ generators/loops/templates/script/loops.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
 require File.dirname(__FILE__) + '/../config/boot'
 require File.dirname(__FILE__) + '/../config/environment'
 
@@ -12,8 +13,48 @@ require 'ext/em'
 require 'ext/emfork'
 require 'loops'
 
+options = { :daemonize => false, :loops => [], :all_loops => false, :list_loops => false }
+opts = OptionParser.new do |opt|
+  opt.banner = "Usage: loops [options]"
+  opt.separator ""
+  opt.separator "Specific options:"
+
+  opt.on('-d', '--daemonize', 'Daemonize when all loops started.') { |v| options[:daemonize] = v }
+  opt.on("--loop=loop_name", 'Start specified loop(s) only') { |v| options[:loops] << v }
+  opt.on('-a', '--all', 'Start all loops') { |v| options[:all_loops] = v }
+  opt.on('-l', '--list', 'Shows all available loops with their options') { |v| options[:list_loops] = v }
+
+  opt.on_tail("-h", "--help", "Show this message") do
+    puts(opt)
+    exit(0)
+  end
+
+  opt.parse!(ARGV)
+end
+
 puts "Loading config..."
 Loops.load_config(LOOPS_CONFIG_FILE)
 
+# List loops if requested
+if options[:list_loops]
+  puts "Available loops:"
+  Loops.config.each do |name, config|
+    puts "Loop: #{name}" + (config['disabled'] ? ' (disabled)' : '')
+    config.each do |k,v|
+      puts " - #{k}: #{v}"
+    end
+  end
+  puts
+  exit(0)
+end
+
+# Ignore --loop options if --all parameter passed
+options[:loops] = :all if options[:all_loops]
+
+# Check what loops we gonna run
+raise "No loops to run!" if options[:loops] == []
+
 puts "Starting workers"
-Loops.start_loops!
+Loops.start_loops!(options[:loops])
+
+# TODO: Make it daemonize if options[:daemonize]
